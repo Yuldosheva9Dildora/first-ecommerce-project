9688b629e767b9b37b092cca3e07a88feea2fe54
Agent-001-lab <mirzaekubovamaskura@gmail.com>
2025-01-03 23:35:29 +0500

    Update layout.jsx

--- ecommerce/src/components/layout/layout.jsx
+++ ecommerce/src/components/layout/layout.jsx
@@ -4,6 +4,44 @@ import Homepage from "../../pages/hompage";
 import AboutPage from "../../pages/about-page";
 import { Route, Routes } from "react-router-dom";
 
+import { Advertisement, Card1, Card2, Card3, Card4, Line, Showcase, Card5, Card6} from "./components/showcase.jsx"
+import { Details, Details1, } from "./components/details.jsx"
+
+import { Route, Routes } from "react-router-dom"
+import { WishlistCards, WishlistCards1 } from "./components/wishlist.jsx"
+
+export const Wishlist = () => {
+  return(
+    <div>
+      <WishlistCards/>
+      <WishlistCards1/>
+    </div>
+  )
+}
+export const CardDetails = () => {
+  return(
+    <div>
+      <Details />
+      <Details1/>
+    </div>
+)}
+
+export const Home = () => {
+  return(
+<div>
+ <Showcase/>
+ <Card1/>
+ <Line/>
+ <Card2/>
+ <Card3/>
+ <Advertisement/>
+ <Card4/>
+ <Card5/>
+ <Card6/>
+</div>
+  )
+}
+
 function Layout() {
   return (
     <>
@@ -11,6 +49,8 @@ function Layout() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<AboutPage />} />
+         <Route path="details/:id" element={<CardDetails />} />
+        <Route path="wishlist" element={<Wishlist/>}/>
       </Routes>
       <Footer />
     </>
